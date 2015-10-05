365a2e954a2477829cd3b35b9af5cbfb4a7022d0
Maksim Karelov <mkarelov@griddynamics.com>
2015-10-05 16:17:29 +0300

    Roll back URL structure from REST style to classic get parameter in CalculatorController

--- src/main/java/com/example/webapp/CalculatorController.java
+++ src/main/java/com/example/webapp/CalculatorController.java
@@ -1,10 +1,7 @@
 package com.example.webapp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
@@ -16,31 +13,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/calc")
 public class CalculatorController {
 
-    @RequestMapping(value = "/multiply/{a:[0-9]+}/{b:[0-9]+}", method = RequestMethod.GET)
+    @RequestMapping(value = "/multiply", method = RequestMethod.GET)
     @ResponseBody
-    public String multiply(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
+    public String multiply(@RequestParam(value = "a", required = true) Integer a,
+                           @RequestParam(value = "b", required = true) Integer b) {
         return String.valueOf(a * b);
     }
 
-    @RequestMapping(value = "/divide/{a:[0-9]+}/{b:[0-9]+}", method = RequestMethod.GET)
+    @RequestMapping(value = "/divide", method = RequestMethod.GET)
     @ResponseBody
-    public String divide(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
+    public String divide(@RequestParam(value = "a", required = true) Integer a,
+                         @RequestParam(value = "b", required = true) Integer b) {
         if (b.equals(0)) {
             return "second operand is zero";
         }
         return String.valueOf(a / b);
     }
 
-    @RequestMapping(value = "/add/{a:[0-9]+}/{b:[0-9]+}", method = RequestMethod.GET)
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     @ResponseBody
-    public String add(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
+    public String add(@RequestParam(value = "a", required = true) Integer a,
+                      @RequestParam(value = "b", required = true) Integer b) {
         return String.valueOf(a + b);
     }
 
     /** Return result of substraction */
-    @RequestMapping(value = "/substract/{a:[0-9]+}/{b:[0-9]+}", method = RequestMethod.GET)
+    @RequestMapping(value = "/substract", method = RequestMethod.GET)
     @ResponseBody
-    public String substract(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
+    public String substract(@RequestParam(value = "a", required = true) Integer a,
+                            @RequestParam(value = "b", required = true) Integer b) {
         return String.valueOf(a - b);
     }
 }
--- src/test/java/com/example/webapp/CalculatorControllerIT.java
+++ src/test/java/com/example/webapp/CalculatorControllerIT.java
@@ -9,21 +9,21 @@ public class CalculatorControllerIT {
 
     @Test
     public void testMultiply() throws Exception {
-        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/multiply/13/3"), "39");
+        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/multiply?a=13&b=3"), "39");
     }
 
     @Test
     public void testDivide() throws Exception {
-        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/divide/33/3"), "11");
+        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/divide?a=33&b=3"), "11");
     }
 
     @Test
     public void testAdd() throws Exception {
-        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/add/13/3"), "16");
+        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/add?a=13&b=3"), "16");
     }
 
     @Test
     public void testSubstract() throws Exception {
-        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/substract/13/3"), "10");
+        assertEquals(HTTPRequestTestHelper.getResponse("http://localhost:8080/calc/substract?a=13&b=3"), "10");
     }
 }
